ef3637f562dc154edcd49e30d2c09d7e6cd152df
Alessandro Caruso <acaruso0@outlook.com>
2024-04-24 17:52:20 +0200

    Added error for OOM

--- src/pointcloud.cc
+++ src/pointcloud.cc
@@ -1,12 +1,23 @@
 #include "pointcloud.h"
 
+#include <cassert>
+#include <cstdlib>
+#include <system_error>
+
+#include "logger.h"
+
 void PointCloud::Allocate(const unsigned int& _nframes, const unsigned int& _npoints) {
+  assert(_nframes > 0 and _npoints > 0);
   if (!x and !y and !z) {
     nframes = _nframes;
     npoints = _npoints;
     x = new double[nframes*npoints];
     y = new double[nframes*npoints];
     z = new double[nframes*npoints];
+  }
+  if (!x or !y or !z) {
+    ERROR(std::make_error_code(std::errc::not_enough_memory).message());
+    std::abort();
   }
 }
 
